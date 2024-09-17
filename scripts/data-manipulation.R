99b7fb5d625718d14980889d71f0619311ab5e6c
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-17 14:41:10 +0100

    Subset countries in Europe

--- scripts/data-manipulation.R
+++ scripts/data-manipulation.R
@@ -14,14 +14,25 @@ africa_mean_gdp
 america_mean_gdp <- mean(gapminder$gdpPercap[gapminder$continent == "America"])
 america_mean_gdp
 
-##########
+##### Create data subsets
 year_country_gdp <- gapminder %>% 
   select(year, country, gdpPercap)
-
 
 smaller_gapminder_data <- gapminder %>% 
   select(-continent)
 
 tidy_gdp <- year_country_gdp %>% 
   rename(gdp_per_capita = gdpPercap)
+
+### filter items
+# annual gdp in Europe
+year_country_gdp_europe <- gapminder %>% 
+  filter(continent == "Europe") %>% 
+  select(-continent)
+
+
+
+
+
+
 
