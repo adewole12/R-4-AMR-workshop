134eac9111600fcb2aff449839473dc1752232fe
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-17 13:10:19 +0100

    added aesthetics and mappings to continent

--- scripts/data-visualisation.R
+++ scripts/data-visualisation.R
@@ -15,11 +15,10 @@ head(gapminder, 5)
 ggplot(
   data = gapminder,
   mapping = aes(y = lifeExp,
-                x = gdpPercap)
-  ) +
-  geom_point()
-
-
+                x = gdpPercap)) +
+  geom_point(alpha = 0.3, mapping = aes(colour = continent)) +
+  scale_x_log10() +
+  geom_smooth(method = "lm", colour = "red", linewidth = 0.4) 
 
 ############trend of life expectancy over time########################
 ggplot(
@@ -27,7 +26,29 @@ ggplot(
   mapping = aes(y = lifeExp,
                 x = year                )
   ) +
-  geom_line(mapping = aes(colour = continent, group = country))
+  geom_line(mapping = aes(colour = continent, group = country))+
+  geom_point(alpha = 0.2) 
+
+
+
+##############################################
+ggplot(
+  data = gapminder[gapminder$country == "Cambodia", ],
+  mapping = aes(y = lifeExp,
+                x = year)) +
+  geom_line(mapping = aes(colour = continent, group = country)) +
+  geom_point()
+
+
+####################################
+ggplot(
+  data = gapminder %>% 
+    filter(country == "Rwanda"),
+  mapping = aes(y = lifeExp,
+                x = year                )
+) +
+  geom_line(mapping = aes(colour = continent, group = country)) +
+  geom_point()
 
 
 
