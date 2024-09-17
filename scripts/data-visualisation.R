de46e538d7eafaf14d8ff17367c4cfd8f1842cdf
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-17 12:12:53 +0100

    start data visualisation script

---
+++ scripts/data-visualisation.R
@@ -0,0 +1,9 @@
+### Load packages
+
+# Load pacman and install the required packages
+library(ggplot2)
+library(readr)
+
+#####Load in the data###############################
+gapminder <- read_csv("data/gapminder_data.csv")
+View(gapminder)
