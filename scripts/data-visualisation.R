d1b1c6a322ca4e84dc1c512808df35f5ed0fb383
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-17 16:54:58 +0100

    doing some advance manipulation and visualisation

--- scripts/data-visualisation.R
+++ scripts/data-visualisation.R
@@ -3,6 +3,7 @@
 # Load pacman and install the required packages
 library(ggplot2)
 library(readr)
+library(tidyverse)
 
 #####Load in the data###############################
 gapminder <- read_csv("data/gapminder_data.csv")
@@ -49,6 +50,62 @@ ggplot(
 ) +
   geom_line(mapping = aes(colour = continent, group = country)) +
   geom_point()
+
+
+### ADVANCED DATA VISUALISATION
+## select data from America
+americas <- gapminder %>% 
+  filter(continent == "Americas")
+
+#americas <- gapminder[gapminder$continent == "Americas", ]
+ggplot(
+  data = americas, mapping = aes(x = year, y = lifeExp)) +
+  geom_line() +
+  theme(axis.text.x = element_text(angle = 45))
+
+
+ggplot(
+  data = americas, mapping = aes(x = year, y = lifeExp)) +
+    geom_line() +
+    facet_wrap(~country) +
+    theme(axis.text.x = element_text(angle = 45))
+
+
+
+gapminder %>% 
+  filter(continent == "Americas") %>% 
+  ggplot(mapping = aes(x = year, y = lifeExp)) +
+  geom_line(aes(colour = country)) +
+  facet_wrap(~country) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  theme(legend.position="none")
+###############################################################
+
+gapminder %>% 
+  mutate(startsWith = substr(country,1,1)) %>% 
+  filter(startsWith %in% c("A", "Z")) %>% 
+  ggplot(aes(x = year, y = lifeExp, colour = continent)) +
+  geom_line() +
+  facet_wrap(vars(country))+ #vars used in place of ~ to allow multiple variables
+  theme_minimal()
+
+###############################
+# Calculate the average life expectancy in 2002 of 2 randomly selected countries
+# for each continent. then arrange the continent names in reverse order.
+# Hint: Use the dplyr functions arrange() and sample_n(),
+# they have similar syntax to other dplyr functions
+
+gapminder %>% 
+  filter(year == 2002) %>% 
+  group_by(continent) %>% 
+  sample_n(2) %>% 
+  summarise(mean_lifeExp = mean(lifeExp)) %>% 
+  arrange(desc(mean_lifeExp))
+
+
+?slice_sample
+
+
 
 
 
