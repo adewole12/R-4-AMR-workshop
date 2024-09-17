726c347d21339c7a5ddca96ec5ad3f31202910af
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-17 14:54:22 +0100

    select, filter and summarise life expectancy in Africa

--- scripts/data-manipulation.R
+++ scripts/data-manipulation.R
@@ -30,8 +30,23 @@ year_country_gdp_europe <- gapminder %>%
   filter(continent == "Europe") %>% 
   select(-continent)
 
+europe_lifeExp_2007 <- gapminder %>% 
+  filter(continent == "Europe", year == 2007) %>% 
+  select(-c(continent, gdpPercap))
 
+gapminder %>% 
+  filter(continent == "Europe")%>% 
+  group_by(country) %>% 
+  summarise(mean_gdp = mean(gdpPercap)) 
 
+### Mean Life Expectancy across African countries
+gapminder %>% 
+  filter(continent == "Africa") %>% 
+  select(-c(continent, pop)) %>% 
+  group_by(country) %>% 
+  summarise(mean_life_exp = mean(lifeExp),
+            highest_life_exp = max(lifeExp),
+            differ_lifeExp = (highest_life_exp - mean_life_exp))
 
 
 
