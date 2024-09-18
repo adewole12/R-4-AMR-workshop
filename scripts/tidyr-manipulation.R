b1fc1ce1c31744e0c58d279a438500c12edb6ea1
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-18 15:16:31 +0100

    Added comments to gap long wrangling code

--- scripts/tidyr-manipulation.R
+++ scripts/tidyr-manipulation.R
@@ -37,22 +37,56 @@ gap_long2 <- gap_wide %>%
   pivot_longer(
     cols = colnames(gap_wide)[-c(1:2)],
     names_to = "obstype_year", 
-    values_to = "obs_values")
-gap_long$year <- as.integer(gap_long$year)
+    values_to = "obs_values") %>% 
+  separate(obstype_year, into = c("obstype", "year"),
+           sep = '_') %>% 
+  mutate(year = as.integer(gap_long2$year))
+
+##########Iterative explanation ############
+# Start with the gap_wide dataframe
+gap_long2 <- gap_wide %>% 
+  # Pivot the dataframe from wide to long format
+  pivot_longer(
+    cols = colnames(gap_wide)[-c(1:2)],  # Select all columns except the first two
+    names_to = "obstype_year",           # Name the new column that will hold the original column names
+    values_to = "obs_values"             # Name the new column that will hold the values
+  ) %>% 
+    # Separate the combined column into two new columns
+  separate(
+    obstype_year,                        # The column to split
+    into = c("obstype", "year"),         # Names of the new columns
+    sep = '_'                            # Character to split on
+  ) %>% 
+    # Convert the year column from character to integer
+  mutate(
+    year = as.integer(year)              # Convert year to integer
+  )
 
 
-waldo::compare(gap_long2,gap_long)
+#####################################
+b <- gap_long %>% 
+  group_by(continent, obstype) %>% 
+  summarise(means = mean(obs_values))
 
 
+a <- gap_long2 %>% 
+  group_by(continent, obstype) %>% 
+  summarise(means = mean(obs_values))
+
+waldo::compare(a,b)
+all.equal(gap_long, gap_long2)
 
 
+##### Convert long to wider format
+gap_normal <- gap_long %>% 
+  pivot_wider(names_from = obstype, values_from = obs_values) 
+
+dim(gap_normal)
+gap_normal <- gap_normal[,names(gapminder)]
 
 
-
-
-
-
-
+waldo::compare(gap_normal, gap_wide)
+all.equal(gap_normal, gap_wide)
 
 
 
