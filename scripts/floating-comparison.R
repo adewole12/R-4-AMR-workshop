293794adc64c192fe1b6226fa3adbdcb6dad9bd7
Adewole Adekola <adewole.adekola@ukhsa.gov.uk>
2024-09-19 10:52:02 +0100

    writing csv files

--- scripts/floating-comparison.R
+++ scripts/floating-comparison.R
@@ -17,3 +17,20 @@ lifeExp_plot
 ggsave("results/life-expectancy-plot.pdf", lifeExp_plot, dpi = 300)
 
 lifeExp_plot
+
+
+
+##############
+gap_australia <- gapminder[gapminder$country =="Australia", ]
+
+
+gap_australia2 <- gapminder %>% 
+  filter(country == "Australia")
+
+write.csv(gap_australia, "results/gapminder-australia.csv",
+          row.names = FALSE)#use this to remove row names 
+
+write.csv(gap_australia2, "results/gapminder-australia2.csv",
+          row.names = FALSE)#use this to remove row names 
+
+waldo::compare(gap_australia, gap_australia2)
